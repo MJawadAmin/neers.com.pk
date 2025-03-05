b23c486aec9f1bd65b82cc26d9f288078dec9630
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-03-05 17:38:36 +0500

    Navbar added pixel by pixel

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi"; // Import icons
 
-import { FaArrowRight } from "react-icons/fa";
+import { GoArrowRight } from "react-icons/go";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
@@ -12,7 +12,7 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <nav className="text-[rgb(166,172,183)] p-4 h-28 ">
-      <div className="container mx-auto flex justify-between items-center px-[120px]">
+      <div className="container mx-auto flex justify-between items-center px-[113px]">
         {/* Logo */}
         <Link href="/" className=" ml-[15.5px] mt-[1px]">
           <Image src="/neeca-logo.webp" alt="Logo" width={68.5} height={68.5}  />
@@ -30,7 +30,7 @@ const Navbar = () => {
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex  ">
-          <ul className="hidden flex-col lg:flex lg:flex-row gap-[39px]  font-[500] text-lg mr-[-3px] mt-[12px] items-start lg:items-center">
+          <ul className="hidden flex-col lg:flex lg:flex-row text-[#9da4b0] gap-[39px]  font-[500] text-lg mr-[-4px] mt-[12px] items-start lg:items-center">
       {[
         { name: "Home", path: "/" },
         { name: "About", path: "/about" },
@@ -50,19 +50,20 @@ const Navbar = () => {
         </li>
       ))}
 
-      <li>
+      <li className="">
         <Link
           href="/register"
-          className="inline-flex items-center gap-2 hover:text-gray-100 text-white bg-[rgb(249,102,46)] px-[13px] py-[6px] rounded-md transition-transform duration-300 hover:-translate-y-2"
+          className="inline-flex items-center gap-2 hover:text-gray-100 text-white bg-[rgb(249,102,46)] px-[12px] py-[6px] rounded-md transition-transform duration-300 hover:-translate-y-2"
 
         >
-          Register a company <FaArrowRight className="text-white text-2xl" />
+          Register a company 
+          <GoArrowRight size={30} className="text-white font-bold" />
         </Link>
       </li>
-      <li>
+      <li className="ml-3">
         <Link
           href="/register"
-          className={`hover:text-bg-[rgb(249,102,46)] ${
+          className={`hover:text-bg-[rgb(249,102,46)]  ${
             pathname === '/signin'? "text-[rgb(249,102,46)] font-semibold" : ""
           }`}>
           Sign in
