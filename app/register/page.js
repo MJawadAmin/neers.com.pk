f96c610c405de1dd5c72d35013ec4458401e5162
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-03-06 19:05:05 +0500

    new commit

--- app/register/page.js
+++ app/register/page.js
@@ -49,7 +49,7 @@ const Register = () => {
         Sign Up
       </h3>
       <div className="flex flex-col md:flex-row h-auto md:h-screen w-full max-w-[91.5vw]  shadow-[0_4px_6px_rgba(170,170,170,0.1)] rounded-lg overflow-hidden">
-        <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-6">
+        <div className="w-full md:w-[50%] flex items-center justify-center bg-white p-6">
           <div className="w-full max-w-md">
             <input type="text" name="fake-user" autoComplete="off" style={{ display: "none" }} />
             <input type="password" name="fake-pass" autoComplete="off" style={{ display: "none" }} />
