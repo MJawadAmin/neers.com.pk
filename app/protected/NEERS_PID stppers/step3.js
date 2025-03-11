f324ec051a9b402299767b24ebca4e65dbf62c7c
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-03-12 03:06:24 +0500

    new cmmits

--- app/protected/NEERS_PID stppers/step3.js
+++ app/protected/NEERS_PID stppers/step3.js
@@ -1,7 +1,7 @@
 export default function Step3({next,  prev }) {
     return (
-      <div className="p-4 text-center">
-        <h2 className="text-2xl font-semibold mb-4">Step 3</h2>
+      <div className=" text-center">
+        <h2 className="text-2xl font-semibold bg-white">Step 3</h2>
         <p>Still Going on !</p>
         <div className="flex gap-4 mt-4">
           <button onClick={prev} className="bg-gray-500 text-white px-4 py-2 rounded">
